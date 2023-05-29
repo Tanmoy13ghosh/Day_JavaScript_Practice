7a8d8279b8d00b3be4a4e0c297e13463155f356e
Tanmoy13ghosh <105385051+Tanmoy13ghosh@users.noreply.github.com>
2023-05-29 23:12:57 +0530

    updated

---
+++ day_1_25_05/index.js
@@ -0,0 +1,78 @@
+let num = 1
+let num2 = '1'
+// console.log(num===num2)
+// console.log(typeof(num2))
+let js = "java"
+let py = "java"
+// console.log(js==py)
+
+var arr = [1,2,3,4];
+//   arr[0] = 10;
+//   console.log(arr);
+
+// let num1 = [1,2,3,4]
+// let number = [1,2,3,4]
+// console.log(num1==number);
+
+// let obj = {
+//     name:"pig"}
+// console.log(obj);
+
+// const pi = 3.14
+// let radious = 100
+// let areaOfCircle = pi*radious*radious
+// console.log(areaOfCircle);
+// console.log(0 == "");
+// console.log(0 ==  false);
+// console.log(1 == true);  
+
+// let number1 = 5;
+// number1>0
+// ?console.log(`${number1}positive number`) 
+// :console.log(`negative number`);
+
+// number = -5;
+// number<0
+// ?console.log(`${number} negative number`)
+// :console.log(`${number} positive number`);
+
+// num = -3;
+// if (num > 0) {
+//   console.log(`${num} is a positive number`);
+// } else {
+//   console.log(`${num} is a negative number`);
+// }
+
+// let a = 4;
+// if(a>0){
+//     console.log(`${a} positive number`);
+// }
+// else if (a<0){
+//     console.log(`${a} negative number`);
+// } else if (a==0){
+//     console.log(`${a} is zero`);
+// }else{
+//     console.log(`${a}it's not a number`);
+// }
+
+let i =0;
+while(i<5){
+    console.log(i);
+    i++
+}
+
+for (let i=0;i<10;i++){
+    console.log(i);
+}
+
+let x=4;
+let y=10
+while(i<y){
+    console.log(i)
+    i++
+}
+
+do{
+    console.log(i);
+    i++;
+}while(i<10);
---
+++ day_2_29_05/index.js
@@ -0,0 +1,56 @@
+// function square(){ //withour parameter
+//     let a = 20;
+//     let b = 10
+//     let c =a*b
+//     console.log(c);
+// }
+// square()
+
+// function name(){
+//     let a = 10
+//     let sq = a*a
+//     console.log(sq)
+//     return sq
+// }
+// var square = name()
+// console.log(square);
+// for(let i=0;i<square;i++)
+// console.log(i);
+
+
+// a= 10
+// b = 0
+// b = a++
+// console.log(b);
+// console.log(a);
+
+// function multiplication(a) { // with parameter
+//      let mul = a*a
+//      console.log(mul);
+// }
+// multiplication(2) //argument
+
+// function multiplication(a) { // with parameter
+//     let mul = a*a
+//     // console.log(mul);
+//     return mul
+// }
+// let solution = multiplication(2)
+// let i = 0;
+// while(i<solution){
+//     console.log(i);
+//     i++
+// }
+
+// function multiplication(a,b) { // with two parameter
+//     let mul = a*b
+//     // console.log(mul);
+//     return mul
+// }
+// let solution = multiplication(20,30)
+// let i = 0;
+// while(i<solution){
+//     console.log(i);
+//     i++
+// }
+
