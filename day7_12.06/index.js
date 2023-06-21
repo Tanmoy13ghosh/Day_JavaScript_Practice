1d9b3966ebbe2d7721a38a06d8a83ae1b457a76f
Tanmoy13ghosh <105385051+Tanmoy13ghosh@users.noreply.github.com>
2023-06-22 00:19:14 +0530

    update

---
+++ day7_12.06/index.js
@@ -0,0 +1,33 @@
+// function concat(){
+// let num1 = 26
+// let num2 = 34
+// const sum = num1+num2
+// return sum;
+// }
+
+// concat()
+// let num1 = 26
+// let num2 = 34
+// const sum = num1+num2
+// console.log(sum);
+
+
+// hello()
+// function hello (){
+//     let a = 10
+//     console.log("hello")
+// }
+// console.log(a)
+
+
+function a(){
+    function b(){
+        function c(){
+            console.log(d);
+        }
+        c()
+    }
+    b()
+}
+a()
+var d = 10
