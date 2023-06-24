9c8b7d49cb68de587f1ede6f516f8cc8993bdad0
Tanmoy13ghosh <105385051+Tanmoy13ghosh@users.noreply.github.com>
2023-06-24 13:57:19 +0530

    day6 commit

--- day6_8_06/index.js
+++ day6_8_06/index.js
@@ -1,4 +1,11 @@
-const arr = [1,2,3,4,5,6]
-arr.push(2,5)
-console.log(arr);
-console.log(arr.indexOf(6))
+// const arr = [1,2,3,4,5,6]
+// arr.push(2,5)
+// console.log(arr);
+// console.log(arr.indexOf(6))
+var hello = {
+    arrowFunction : () => {
+        var hh = 10
+        return hh
+    }
+}
+console.log(hello.arrowFunction())
